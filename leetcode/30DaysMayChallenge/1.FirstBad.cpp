fbb219967f4a70083ec2df1bfe204110ba021297
Baibhav <baibhab@tcs.com>
2020-05-04 11:15:52 +0530

    Leetcode 30 May Challenge 1,2

---
+++ leetcode/30DaysMayChallenge/1.FirstBad.cpp
@@ -0,0 +1,48 @@
+// You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+// Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+// You are given an API bool isBadVersion(version) which will return whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+// Example:
+
+// Given n = 5, and version = 4 is the first bad version.
+
+// call isBadVersion(3) -> false
+// call isBadVersion(5) -> true
+// call isBadVersion(4) -> true
+
+// Then 4 is the first bad version.
+
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution
+{
+  public:
+    int firstBadVersion(int n)
+    {
+        int i = 1, j = n;
+        if (isBadVersion(i))
+            return i;
+        while (i <= j)
+        {
+            int mid = i + (j - i) / 2;
+            bool t = isBadVersion(mid);
+            bool prev = isBadVersion(mid - 1);
+            if (t && !prev)
+            {
+                return mid;
+            }
+            if (t)
+            {
+                j = mid - 1;
+            }
+            else
+            {
+                i = mid + 1;
+            }
+        }
+        return n;
+    }
+};
